ce3b53c85841a9f0ccb554283d0017e7605cb1aa
Sam Evans <evansst@icloud.com>
2020-07-08 17:46:46 -0600

    Adds add_fantasy_team instance method to FantasyLeague class.

--- app/models/fantasy_league.rb
+++ app/models/fantasy_league.rb
@@ -2,4 +2,9 @@ class FantasyLeague < ActiveRecord::Base
   has_many :fantasy_teams
   has_many :users, through: :fantasy_teams
 
+  def add_fantasy_team user, fantasy_team_name
+    new_team = user.create_fantasy_team(fantasy_team_name)
+    fantasy_teams << new_team
+  end
+
 end

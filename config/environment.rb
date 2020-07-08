be9ec6131873e08730c8c960bc1fc6f8867442e4
Sam Evans <evansst@icloud.com>
2020-07-08 12:50:02 -0600

    Adds sports teams seeds.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ Bundler.require
 require_all 'lib'
 require_all 'app/models'
 
+
 ENV["SINATRA_ENV"] ||= 'development'
 ActiveRecord::Base.establish_connection(ENV["SINATRA_ENV"].to_sym)
 # connection_details = YAML::load(File.open('config/database.yml'))
---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+League.delete_all
+User.delete_all
+Team.delete_all
+SportsTeam.delete_all
+
+bills = SportsTeam.create(name: "Buffalo Bills")
+dolphins = SportsTeam.create(name: "Miami Dolphins")
+patriots = SportsTeam.create(name: "New England Patriots")
+jets = SportsTeam.create(name: "New York Jets")
+
+ravens = SportsTeam.create(name: "Baltimore Ravens")
+bengals = SportsTeam.create(name: "Cincinnati Bengals")
+browns = SportsTeam.create(name: "Cleveland Browns")
+steelers = SportsTeam.create(name: "Pittsburgh Steelers")
+
+texans = SportsTeam.create(name: "Houston Texans")
+colts = SportsTeam.create(name: "Indianapolis Colts")
+jaguars = SportsTeam.create(name: "Jacksonville Jaguars")
+titans = SportsTeam.create(name: "Tennesee Titans")
+
+broncos = SportsTeam.create(name: "Denver Broncos")
+chiefs = SportsTeam.create(name: "Kansas City Chiefs")
+raiders = SportsTeam.create(name: "Las Vegas Raiders")
+chargers = SportsTeam.create(name: "Los Angeles Chargers")
+
+cowboys = SportsTeam.create(name: "Dallas Cowboys")
+giants = SportsTeam.create(name: "New York Giants")
+eagles = SportsTeam.create(name: "Philadelphia Eagles")
+redskins = SportsTeam.create(name: "Washinton Redskins")
+
+bears = SportsTeam.create(name: "Chicago Bears")
+lions = SportsTeam.create(name: "Detroit Lions")
+packers = SportsTeam.create(name: "Green Bay Packers")
+vikings = SportsTeam.create(name: "Minnesota Vikings")
+
+falcons = SportsTeam.create(name: "Atlanta Falcons")
+panthers = SportsTeam.create(name: "Carolina Panthers")
+saints = SportsTeam.create(name: "New Orleans Saints")
+buccaneers = SportsTeam.create(name: "Tampa Bay Buccaneers")
+
+cardinals = SportsTeam.create(name: "Arizona Cardinals")
+rams = SportsTeam.create(name: "Los Angeles Rams")
+fortyniners = SportsTeam.create(name: "San Fransisco 49ers")
+seahawks = SportsTeam.create(name: "Seattle Seahawks")
